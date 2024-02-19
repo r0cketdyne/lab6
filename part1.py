831c30f40dfa4a497bcca5c22370940a81da8265
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-19 14:36:59 -0500

    small steps

--- part1.py
+++ part1.py
@@ -2,16 +2,16 @@
 # Matthew Stephenson
 # This program computes factorial for a value (N) entered by the user.
 # STEP 1: Prompt the user for N and read it in and convert it into an integer\
-    
+    var_fact = input("Throw me an integer...we're going to add bang and compute the resulting factorial")
 # STEP 2: Initialize factorial to a default value before multiplying
 
 # values into it
 # STEP 3: Complete the following loop to compute factorial
 
-while N > : # STEP 3a: what is the condition to keep going?
+while var_fact > 0 : # STEP 3a: what is the condition to keep going?
 
     # STEP 3b: multiply N x factorial and save it back into factorial variable
-
+    var_fact * 
     # STEP 3c: decrement N by 1
 
 # Step 4: Print out The resulting factorial value
