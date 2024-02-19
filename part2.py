8ac421939b2b86ca75ebfeed36652bf870a3fe3e
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-19 14:38:26 -0500

    mathy

---
+++ part2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 14:37:56 2024
+
+@author: stephenson
+"""
+
+# Lab6 - Part II
+#Matthew Stephenson
+# This program reads in numbers until the user enters a 0. For each number
+# entered, prints whether it is even or odd.
+number = int(input("Enter a number. I will tell you if it is odd or even: "))
+while : # STEP 1: condition that allows the loop to continue
+# STEP 2: print out whether the number is odd or even
+# STEP 3: prompt the user for the next number
+print("\nYou entered 0. Goodbye")
+
+"""
+Finishing Up:
+Upload both of your program files for this lab submission. (It is permissible to put them both into the
+same program file if you wish.)
+Enter a number. I will tell you if it is odd or even: 8
+The number 8 is even.
+Please enter another number, 0 to stop: 187
+The number 187 is odd.
+Please enter another number, 0 to stop: -17
+The number -17 is odd.
+Please enter another number, 0 to stop: 0
+You entered 0. Goodbye
+"""
