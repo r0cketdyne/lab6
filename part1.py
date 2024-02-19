becdd0f70acfc7f0b6f7aa4188e1e5db2891a3b9
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-19 16:30:34 -0500

    progresss

--- part1.py
+++ part1.py
@@ -2,7 +2,9 @@
 # Matthew Stephenson
 # This program computes factorial for a value (N) entered by the user.
 # STEP 1: Prompt the user for N and read it in and convert it into an integer\
-    var_fact = input("Throw me an integer...we're going to add bang and compute the resulting factorial")
+var_fact = float(input("Throw me an integer...we're going to add bang and compute the resulting factorial"))
+var_arr = []
+var_sum = 0
 # STEP 2: Initialize factorial to a default value before multiplying
 
 # values into it
@@ -11,8 +13,11 @@
 while var_fact > 0 : # STEP 3a: what is the condition to keep going?
 
     # STEP 3b: multiply N x factorial and save it back into factorial variable
-    var_fact * 
+     var_sum += (var_fact * var_fact - 1)
+     var_fact -=1
     # STEP 3c: decrement N by 1
+    
 
 # Step 4: Print out The resulting factorial value
 #this is kind of mathy ngl
+print(var_sum)
