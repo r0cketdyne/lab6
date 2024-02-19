637d7328a7bd79f6cdeb1a1bf0aff5132b951cc7
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-19 16:49:58 -0500

    refined

--- part2.py
+++ part2.py
@@ -1,15 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 19 14:37:56 2024
-
-@author: stephenson
-"""
-
-#we are going to define some funcs...yay! sadly they are not built in
-
 # Lab6 - Part II
-#Matthew Stephenson
+# Matthew Stephenson
 # This program reads in numbers until the user enters a 0. For each number
 # entered, prints whether it is even or odd.
 
@@ -21,28 +11,14 @@ def is_odd(number):
 #the function definition above sadly isnt inluded in the math lib. so we made it here
 
 number = int(input("Enter a number. I will tell you if it is odd or even: "))
-while number != 0: # STEP 1: condition that allows the loop to continue
-# STEP 2: print out whether the number is odd or even
+
+while number != 0:
     if is_even(number):
-        print("the number is even")
-
+        print(f"The number {number} is even.")
     elif is_odd(number):
-        print("the number is odd")
-# STEP 3: prompt the user for the next number
+        print(f"The number {number} is odd.")
+    
+    # Prompt the user for the next number inside the loop
+    number = int(input("Please enter another number, 0 to stop: "))
+
 print("\nYou entered 0. Goodbye")
-
-"""
-Finishing Up:
-Upload both of your program files for this lab submission. (It is permissible to put them both into the
-same program file if you wish.)
-Enter a number. I will tell you if it is odd or even: 8
-The number 8 is even.
-Please enter another number, 0 to stop: 187
-The number 187 is odd.
-Please enter another number, 0 to stop: -17
-The number -17 is odd.
-Please enter another number, 0 to stop: 0
-You entered 0. Goodbye
-"""
-
-#yay everything is local going to get food and come back slightly later. exam prep after this yay
