7f43509c75bb5ba1f17d9e178cc0fd536ac5ac79
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-19 14:05:13 -0500

    guess I'm going to write this from scratch. Kind of. there's a wireframe here which is chill. should help us memorize correct prog struct

---
+++ part1.py
@@ -0,0 +1,15 @@
+# Lab 6 - Part I
+# Matthew Stephenson
+# This program computes factorial for a value (N) entered by the user.
+# STEP 1: Prompt the user for N and read it in and convert it into an integer\
+    
+# STEP 2: Initialize factorial to a default value before multiplying
+
+# values into it
+# STEP 3: Complete the following loop to compute factorial
+
+while N > : # STEP 3a: what is the condition to keep going?
+
+# STEP 3b: multiply N x factorial and save it back into factorial variable
+# STEP 3c: decrement N by 1
+# Step 4: Print out The resulting factorial value
